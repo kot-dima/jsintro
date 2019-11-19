727ab96bd6691d0cf3e060cb2b058a29a97f489b
Дмитро Кот <Kot@rivne.itstep.org>
2019-11-19 21:09:53 +0200

    2 add

---
+++ js/index.js
@@ -0,0 +1,7 @@
+var post=document.querySelector('.post');
+var but=document.querySelector('button');
+var search=document.querySelector('.search');
+/*but.addEventListener('onclick', function(){
+
+})*/
+post 
