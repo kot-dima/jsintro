5350b4bb5a2c15b719568bf21701e6c4eae7e84b
Дмитро Кот <Kot@rivne.itstep.org>
2019-11-21 18:30:15 +0200

    home1

--- js/index.js
+++ js/index.js
@@ -1,7 +1,20 @@
 var post=document.querySelector('.post');
 var but=document.querySelector('button');
-var search=document.querySelector('.search');
-/*but.addEventListener('onclick', function(){
+var search=document.querySelector('#search');
+but.addEventListener('click', load);
 
-})*/
-post 
+function load(){
+    var contain=search.value;
+    var url=`http://www.omdbapi.com/?i=tt3896198&apikey=67c1e8cd&t=${contain}`;
+    fetch(url)
+    .then(function(responce){
+        return responce.json();
+    })
+    .then(function(data){
+        console.log(data)
+            document.querySelector('h1').innerHTML = data.Title;
+            document.querySelector('h5').innerHTML = data.Year;
+            var poste = document.querySelector('.poste');
+            poste.innerHTML = `<img src="${data.Poster}" alt="soryanich">`
+    })
+}
